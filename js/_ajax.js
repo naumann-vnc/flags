79bdfc4ec97c8a55e259b6a8c94effc45c7c17ca
naumann-vnc <naumannvnc@gmail.com>
2022-12-04 23:41:14 -0300

    update 20221204

--- js/_ajax.js
+++ js/_ajax.js
@@ -1,9 +1,39 @@
 $(function () {
-    var countries = ["ZWE", "ZMB", "YEM", "ESH", "WLF", "VIR", "VNM", "VEN", "VAT", "VUT", "UZB", "USA", "URY", "UKR", "GBR", "UGA", "ARE", "TUV", "TCA", "TKM", "TUR", "TUN", "TTO", "TON", "TKL", "TGO", "THA", "TZA", "TJK", "TWN", "SYR", "CHE", "SWE", "SWZ", "SJM", "SUR", "SDN", "VCT", "SPM", "MAF", "LCA", "KNA", "SHN", "LKA", "ESP", "SSD", "KOR", "ZAF", "SOM", "SLB", "SVN", "SVK", "SXM", "SGP", "SLE", "SYC", "SRB", "SEN", "SAU", "STP", "SMR", "WSM", "BLM", "RWA", "RUS", "ROU", "REU", "QAT", "PRI", "PRT", "POL", "PCN", "PHL", "PER", "PRY", "PNG", "PAN", "PSE", "PLW", "PAK", "OMN", "NOR", "PRK", "MNP", "NIU", "NGA", "NER", "NIC", "NZL", "NCL", "NLD", "NPL", "NRU", "NAM", "MOZ", "MAR", "MSR", "MNE", "MNG", "MCO", "MDA", "FSM", "MEX", "MYT", "MUS", "MRT", "MHL", "MLT", "MLI", "MDV", "MYS", "MWI", "MDG", "MKD", "MAC", "LUX", "LTU", "LIE", "LBY", "LBR", "LSO", "LBN", "LVA", "LAO", "KGZ", "KWT", "KIR", "KEN", "KAZ", "JOR", "JEY", "JPN", "JAM", "CIV", "ITA", "ISR", "IMN", "IRL", "IRQ", "IRN", "IDN", "IND", "ISL", "HUN", "HKG", "HND", "HTI", "GUY", "GNB", "GIN", "GGY", "GTM", "GUM", "GRD", "GRL", "GRC", "GIB", "GHA", "DEU", "GEO", "GMB", "GAB", "PYF", "FRA", "FIN", "FJI", "FRO", "FLK", "ETH", "EST", "ERI", "GNQ", "SLV", "EGY", "ECU", "TLS", "DOM", "DMA", "DJI", "DNK", "CZE", "CYP", "CUW", "CUB", "HRV", "CRI", "COK", "COD", "COG", "COM", "COL", "CCK", "CXR", "CHN", "CHL", "TCD", "CAF", "CYM", "CPV", "CAN", "CMR", "KHM", "BDI", "MMR", "BFA", "BGR", "BRN", "VGB", "IOT", "BRA", "BWA", "BIH", "BOL", "BTN", "BMU", "BEN", "BLZ", "BEL", "BLR", "BRB", "BGD", "BHR", "BHS", "AZE", "AUT", "AUS", "ABW", "ARM", "ARG", "ATG", "ATA", "AIA", "AGO", "AND", "ASM", "DZA", "ALB", "AFG"]
+    var countries = [ "af", "ax", "al", "dz", "as", "ad", "ao", "ai", "aq", "ag", "ar", "am", "aw", "au", "at", "az", "bs", "bh", "bd", "bb", "by", "be", "bz", "bj", "bm", "bt", "bo", "bq", "ba", "bw", "bv", "br", "io", "bn", "bg", "bf", "bi", "cv", "kh", "cm", "ca", "ky", "cf", "td", "cl", "cn", "cx", "cc", "co", "km", "cd", "cg", "ck", "cr", "ci", "hr", "cu", "cw", "cy", "cz", "dk", "dj", "dm", "do", "ec", "eg", "sv", "gq", "er", "ee", "sz", "et", "fk", "fo", "fj", "fi", "fr", "gf", "pf", "tf", "ga", "gm", "ge", "de", "gh", "gi", "gr", "gl", "gd", "gp", "gu", "gt", "gg", "gn", "gw", "gy", "ht", "hm", "va", "hn", "hk", "hu", "is", "in", "id", "ir", "iq", "ie", "im", "il", "it", "jm", "jp", "je", "jo", "kz", "ke", "ki", "kp", "kr", "kw", "kg", "la", "lv", "lb", "ls", "lr", "ly", "li", "lt", "lu", "mo", "mk", "mg", "mw", "my", "mv", "ml", "mt", "mh", "mq", "mr", "mu", "yt", "mx", "fm", "md", "mc", "mn", "me", "ms", "ma", "mz", "mm", "na", "nr", "np", "nl", "nc", "nz", "ni", "ne", "ng", "nu", "nf", "mp", "no", "om", "pk", "pw", "ps", "pa", "pg", "py", "pe", "ph", "pn", "pl", "pt", "pr", "qa", "re", "ro", "ru", "rw", "bl", "sh", "kn", "lc", "mf", "pm", "vc", "ws", "sm", "st", "sa", "sn", "rs", "sc", "sl", "sg", "sx", "sk", "si", "sb", "so", "za", "gs", "ss", "es", "lk", "sd", "sr", "sj", "se", "ch", "sy", "tw", "tj", "tz", "th", "tl", "tg", "tk", "to", "tt", "tn", "tr", "tm", "tc", "tv", "ug", "ua", "ae", "gb", "um", "us", "uy", "uz", "vu", "ve", "vn", "vg", "vi", "wf", "eh", "ye", "zm", "zw" ]
     var randomCountry = countries[Math.floor(Math.random() * countries.length)];
     var correctCountryG = "";
     var score = 0;
+    var jsonData = {"email": getCookie('email'),"hit":0,"miss":0,"attempts":0};
 
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    }
+    function apiCall(resource, method, body) {
+        $.ajax('http://18.209.223.63:8090' + resource, {
+            contentType: 'application/json',
+            type: method,
+            data: body,
+            success: function (dados, textStatus, xhr) {
+                console.log(xhr.status);
+
+                //return xhr.status
+                //if (xhr.status === '200') {
+                //}
+                //console.log(dados);
+                //return -1;
+            },
+            complete: function () {
+                //$('.loading').remove();
+            }
+        })
+    }
     function obterDados() {
         function mostrarFlag(dados) {
             //console.log(dados);
@@ -43,7 +73,8 @@ $(function () {
         }
         var numRandoms = 5;
         var uniqueRandoms = [];
-        var fflag = "https://countryflagsapi.com/svg/" + randomCountry
+        //var fflag = "https://countryflagsapi.com/svg/" + randomCountry
+        var fflag = "https://flagcdn.com/" + randomCountry + ".svg"
         for (let i = 1; i <= 4; i++) {
             if (!uniqueRandoms.length) {
                 for (var j = 1; j < numRandoms; j++) {
@@ -67,6 +98,8 @@ $(function () {
         $("#btn1, #btn2, #btn3, #btn4").click(function () {
             scorePoint($(this).attr('id'));
         });
+        console.log(getCookie('email'));
+        
         function setProgress(value) {
             score += value;
             $("#progress-bar").css("width", score + '%');
@@ -77,8 +110,13 @@ $(function () {
             }
             if (score == 100) {
                 $('#myModal').modal('show');
+                //console.log("!!!!!!!"+ JSON.stringify(jsonData));
+                apiCall('/scores', 'POST', JSON.stringify(jsonData))
                 $('#btnYes').click(function() {
                     location.reload();
+                });
+                $('#btnNo').click(function() {
+                    window.location.href = "reports.html";
                 });
             }
             obterDados();
@@ -86,11 +124,14 @@ $(function () {
         function scorePoint(btn) {
             if (correctCountryG == $('#' + btn).attr('alt')) {
                 //console.log($('#' + btn).attr('alt'));
-                setProgress(+10);
+                jsonData['hit'] += 1;
+                jsonData['attempts'] += 1;
+                setProgress(+100);
             } else {
+                jsonData['miss'] += 1;
+                jsonData['attempts'] += 1;
                 setProgress(-10);
             }
-            //console.log(score);
         }
     });
     obterDados();
--- js/api_control.js
+++ js/api_control.js
@@ -1,21 +1,68 @@
 $(function () {
+    function setCookie(name,value,days) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days*24*60*60*1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    }
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    }
+    function eraseCookie(name) {   
+        document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    }
 
-    function apiCall(resource, method, body) {
-        return Promise.resolve($.ajax('http://127.0.0.1:5000' + resource, {
+    function apiCallCode(resource, method, body) {
+        var jqXHR = $.ajax('http://18.209.223.63:8080' + resource, {
             contentType: 'application/json',
             type: method,
             data: body,
-            beforeSend: function () {
-                //$('.table').after('<p class="loading"> carregando ... </p>')
+            success: function (data, textStatus, xhr) {
+                return xhr.status
             },
-            error: function () {
-                //$('.table').after('<p class="loading"> deu ruim </p>')
+            async: false
+        });
+        //console.log(jqXHR.responseText);
+        //console.log(jqXHR.status);
+        return jqXHR;
+    }
+    function getScore(method, body) {
+        return Promise.resolve($.ajax('http://18.209.223.63:8090/scores', {
+            contentType: 'application/json',
+            type: method,
+            data: body,
+            //headers: {
+            //    "Authorization": "Bearer " + token
+            //},
+            success: function (dados, textStatus, xhr) {
+                console.log(xhr.status);
+            }
+        }))
+    }
+    function apiCall(resource, method, body, token) {
+        return Promise.resolve($.ajax('http://18.209.223.63:8080' + resource, {
+            contentType: 'application/json',
+            type: method,
+            data: body,
+            headers: {
+                "Authorization": "Bearer " + token
             },
             success: function (dados, textStatus, xhr) {
-                //console.log(xhr.status);
-                if (xhr.status === '200') {
-                    //return dados;
-                }
+                console.log(xhr.status);
+
+                //return xhr.status
+                //if (xhr.status === '200') {
+                //}
                 //console.log(dados);
                 //return -1;
             },
@@ -25,26 +72,89 @@ $(function () {
         }))
     }
 
-    //}
     async function logar(login_info) {
         let data = await apiCall("/login", "POST", login_info);
+        try {
+            console.log('Data:', data);
+            //console.log("!!!!!!!!!!!"+ apiCallCode("/auth", "POST", login_info).status);
+            //console.log("!!!!!!!!!!!"+ apiCall("/auth", "POST", login_info, data.access_token));
+            //console.log(data.access_token);
+            //console.log(apiCall("/auth", "POST", login_info, data.access_token).status);
+            if (await check_auth(login_info, data.access_token)) {
+                setCookie('email', JSON.parse(login_info).login, 1);
+                console.log(getCookie('email'));
+                //window.sessionStorage.setItem('email', JSON.parse(login_info).login);
+                //console.log(JSON.parse(login_info).login);
+                //console.log(window.sessionStorage.getItem('email'));
+                window.location.href = "home.html";
+            }
+        } catch (error) {
+            console.log('Error:', error);
+        }
+
+    }
+    async function check_auth(login_info, token) {
+        let api_return_code = await apiCall("/auth", "POST", login_info, token);
+        try {
+            console.log('code:', api_return_code);
+            if (api_return_code.auth == "ok") {
+                return true;
+            }
+        } catch (error) {
+            return false;
+        }
+        return false;
+    }
+    async function create_user(body) {
+        let data = await apiCall("/users", "POST", body);
         try {
             console.log('Data:', data);
             test_string = String(data)
         } catch (error) {
             console.log('Error:', error);
         }
-        const regex = /{access_token:(.*)'}/g;
         const found = String(data).match(regex);
         if (test_string.match(regex)) {
-            
-            console.log('found!!!:',found);
+
+            console.log('found!!!:', found);
         }
         console.log(found);
 
     }
     $(document).ready(function () {
-
+        console.log(getCookie('email'));
+        jsonObj = {"email": getCookie('email')}
+        console.log(jsonObj);
+        console.log(getScore('GET', JSON.stringify(jsonObj)));
+        $("#test").text(function( index ) {
+            return window.sessionStorage.getItem('email');
+          });
+        $("#play_btn").on({
+            mouseenter: function () {
+                $('.icon_highlight').css("color", "#4169e1");
+            },
+            mouseleave: function () {
+                $('.icon_highlight').css("color", "#000000");
+            },
+            click: function () {
+                window.location.href = "index.html";
+            }
+        });
+        $("#report_btn").on({
+            mouseenter: function () {
+                $('#play_icon').css("color", "#4169e1");
+            },
+            mouseleave: function () {
+                $('#play_icon').css("color", "#000000");
+            },
+            click: function () {
+                window.location.href = "reports.html";
+            }
+        });
+        //$('#phone-number').mask('0000-0000');
+        //if (! check_auth(login_info, data.access_token)) {
+        //    window.location.href="login.html";
+        //}
         $("#btn_login").click(function () {
             //console.log("teste");
             var $items = $('#login, #password')
@@ -58,7 +168,18 @@ $(function () {
             logar(json);
             //scorePoint($(this).attr('id'));
         });
+        $("#btn_signup").click(function () {
+            var $items = $('#email, #password')
+            var obj = { "user_id": 27 }
+            $items.each(function () {
+                obj[this.id] = $(this).val();
+            })
+            var json = JSON.stringify(obj);
 
+            console.log(json);
+            create_user(json);
+
+        });
     });
     //execution();
 });
